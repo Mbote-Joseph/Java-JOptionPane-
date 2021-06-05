1e51d9d4e598d88f4d81bf4d835cca19e2c463ad
Mbote-Joseph <mbotejoseph001@gmail.com>
2021-06-05 23:25:09 +0300

    clock updates

--- Assignment2/Clock.java
+++ Assignment2/Clock.java
@@ -2,9 +2,18 @@ package Assignment2;
 
 public class Clock {
     private int hr=0;
+    private int min=0;
+    private int sec=0;
 
     public Clock(){
         hr= 10;
+        min=30;
+        sec=55;
+    }
+    public Clock(int h, int m, int s){
+        hr= h;
+        min=m;
+        sec=s;
     }
     public int getHour(){
         return hr;
