8f6d89a7eaac65aacf714f88221d285c6c6c80fd
Mbote-Joseph <mbotejoseph001@gmail.com>
2021-06-06 22:47:23 +0300

    OOP update

---
+++ Assignment2/Vehicle.java
@@ -0,0 +1,29 @@
+package Assignment2;
+
+public class Vehicle {
+    private String name="";
+    private int wheels=0;
+    private String color="";
+
+
+    public Vehicle(){
+        name="Bus";
+        wheels=6;
+        color="white";
+    }
+    public Vehicle(String n, int w, String c){
+        name=n;
+        wheels=w;
+        color=c;
+    }
+    public String toString(){
+        String str= "The "+name+" vehicle has a color of "+color+ " and has "+ wheels +" wheels.";
+        return str;
+    }
+    public static void main(String[] args){
+        Vehicle lambo= new Vehicle("Lamborghini", 4, "Red");
+        lambo.toString();
+        System.out.println(lambo.toString());
+    }
+    
+}
